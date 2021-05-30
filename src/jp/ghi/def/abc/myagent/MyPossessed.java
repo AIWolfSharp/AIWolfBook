5a91effc74691f46e5c79c082e7916afd3c3b31e
otsuki <takashi_otsuki@outlook.com>
2021-05-30 22:57:49 +0900

    MyPossessed: fix to prevent null reference exception

--- src/jp/ghi/def/abc/myagent/MyPossessed.java
+++ src/jp/ghi/def/abc/myagent/MyPossessed.java
@@ -114,12 +114,19 @@ public class MyPossessed extends MyVillager {
 				}
 			}
 		}
-		if (!candidates.contains(voteCandidate)) {
-			voteCandidate = randomSelect(candidates);
-			// 以前の投票先から変わる場合，新たに推測発言と占い要請をする
-			if (canTalk) {
-				talkQueue.offer(new Content(new EstimateContentBuilder(voteCandidate, Role.WEREWOLF)));
-				talkQueue.offer(new Content(new RequestContentBuilder(null, new Content(new DivinationContentBuilder(voteCandidate)))));
+		// 候補がいない場合はランダム
+		if (candidates.isEmpty()) {
+			if (!aliveOthers.contains(voteCandidate)) {
+				voteCandidate = randomSelect(aliveOthers);
+			}
+		} else {
+			if (!candidates.contains(voteCandidate)) {
+				voteCandidate = randomSelect(candidates);
+				// 以前の投票先から変わる場合，新たに推測発言と占い要請をする
+				if (canTalk) {
+					talkQueue.offer(new Content(new EstimateContentBuilder(voteCandidate, Role.WEREWOLF)));
+					talkQueue.offer(new Content(new RequestContentBuilder(null, new Content(new DivinationContentBuilder(voteCandidate)))));
+				}
 			}
 		}
 	}
